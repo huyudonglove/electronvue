dadabbe9558e3cc79ef6d8e914b51625cd9db439
huyudong <379145942@qq.com>
2020-05-22 18:15:23 +0800

    init

---
+++ src/renderer/components/aa.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+       222222222
+    </div>
+</template>
+<script>
+export default {
+    name:'aa',
+
+}
+</script>
---
+++ src/renderer/router/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import aa from '../components/aa.vue'
+
+Vue.use(Router)
+const originalPush = Router.prototype.push
+Router.prototype.push = function push (location) {
+  return originalPush.call(this, location).catch(err => err)
+}
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'landing-page',
+      component: require('@/components/LandingPage').default,
+      children:[
+        {
+          path:'/aa',
+          name:'aa',
+          component:aa
+        }
+      ]
+    },
+    {
+      path: '*',
+      redirect: '/'
+    }
+  ]
+})
