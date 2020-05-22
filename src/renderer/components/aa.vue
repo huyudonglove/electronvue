f7074d6ce00ae6e5b5682113b632cd3790d345f1
huyudong <379145942@qq.com>
2020-05-22 18:34:21 +0800

    msg

--- src/renderer/components/aa.vue
+++ src/renderer/components/aa.vue
@@ -1,11 +1,21 @@
 <template>
     <div>
        222222222
+       <button @click="readF">123</button>
     </div>
 </template>
 <script>
+var fs=require('fs');
 export default {
     name:'aa',
+    methods:{
+        readF(){
+            fs.readFile('../../static/11.json','utf-8',function(err,data){
+                console.log(err)
+                console.log(data)
+            })
+        }
+    }
 
 }
 </script>
